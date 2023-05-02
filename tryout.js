9f18825bc23a7f5acd6e25a8ce55985d644bae43
oladee <deemajor230600@gmail.com>
2023-05-02 19:43:09 +0100

    updating the UI

--- tryout.js
+++ tryout.js
@@ -5,17 +5,40 @@ const annually = [
         storage: "500 GB Storage",
         users: "2 Users Allowed",
         fileSize: "Send uP to 3GB"
-    }
+    },
     {
         plan: 'Professional',
         price: "249.99",
         storage: "1 TB Storage",
         users: "3 Users Allowed",
         fileSize: "Send up to 10 GB"
-    }
+    },
     {
         plan: 'Master',
         price: "399.99",
+        storage: "2 TB Storage",
+        users: "10 Users Allowed",
+        fileSize: "Send up to 20GB"
+    }
+]
+const monthly = [
+    {
+        plan: 'Basic',
+        price: "19.99",
+        storage: "500 GB Storage",
+        users: "2 Users Allowed",
+        fileSize: "Send uP to 3GB"
+    },
+    {
+        plan: 'Professional',
+        price: "24.99",
+        storage: "1 TB Storage",
+        users: "5 Users Allowed",
+        fileSize: "Send up to 10 GB"
+    },
+    {
+        plan: 'Master',
+        price: "39.99",
         storage: "2 TB Storage",
         users: "10 Users Allowed",
         fileSize: "Send up to 20GB"
